d3cefeb7fb069704adb425b2750ee5d6cb1bf188
Daniel Parreira <daniel@Daniels-MacBook-Pro.local>
2017-02-12 19:37:46 -0200

    Support Twilio

--- README.md
+++ README.md
@@ -9,8 +9,11 @@ https://www.ibm.com/support/knowledgecenter/en/SSTPQH_1.0.0/com.ibm.cloudant.loc
 	> git clone git@github.com:BluemixDemoApp/CreditCardTransactionAPI.git
 	> cd CreditCardTransactionAPI
 	> touch .env
-    > echo "cloudant_username=${MY_USER}" >>  .env     
-    > echo "cloudant_password='${MY_PASSWORD}'"  >> .env   
+    > echo "cloudant_username='${CLOUDANT_USER}'" >>  .env     
+    > echo "cloudant_password='${CLOUDANT_PASSWORD}'"  >> .env   
+    > echo "twilio_account_sid='${TWILIO_ACCOUNT_SID}'"  >> .env   
+    > echo "twilio_auth_token='${TWILIO_AUTH_TOKEN}'"  >> .env   
+    > echo "twilio_phone_number='${TWILIO_PHONE_NUMBER}'"  >> .env   
 	> npm install
 	> npm start
 ```
--- services/TwilioService.js
+++ services/TwilioService.js
@@ -1,25 +1,21 @@
 // require the Twilio module and create a REST client
 const env = process.env;
 const client = require('twilio')(env.twilio_account_sid, env.twilio_auth_token);
+const Q = require('q');
 
 //Send an SMS text message
 exports.sendMessage = function(to, message) {
     const deferred = Q.defer();
 
-    //Send an SMS text message
     client.sendMessage({
         to:'+1' + to, // Any number Twilio can deliver to
         from: '+1' + env.twilio_phone_number, // A number you bought from Twilio and can use for outbound communication
-        body: message // body of the SMS message
-    }, function(err, responseData) { //this function is executed when a response is received from Twilio
-
+        body: message
+    }).then(function(responseData) {
         console.log(responseData);
-
-        if (!err) { 
-            deferred.resolve(responseData);
-        } else {
-            deferred.reject(err);
-        }
+        deferred.resolve(responseData);
+    }).fail(function(error) {
+        deferred.reject(error);
     });
 
     return deferred.promise;
